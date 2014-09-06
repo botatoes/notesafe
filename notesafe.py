4260a7db560651d7e65a2a2d1d3d84999135482a
Leo Xuzhang Lin <xuzhang@umass.edu>
2014-09-06 10:23:13 -0400

    Index

--- notesafe.py
+++ notesafe.py
@@ -1,8 +1,10 @@
 import os
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-	    return 'Hello World!'
+def home():
+	return render_template('/content/index.html', title='Note Safe - A anonymous note storage app')
+
