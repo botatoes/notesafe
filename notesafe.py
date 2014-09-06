f9c32110f751772f17f88a47a92ae5868bb55900
Leo Xuzhang Lin <xuzhang@umass.edu>
2014-09-06 14:23:40 -0400

    Modified some API

--- notesafe.py
+++ notesafe.py
@@ -8,3 +8,24 @@ app = Flask(__name__)
 def root():
 	return render_template('index.html')
 
+# The login API
+# Requires a POST request on the url /api-login
+
+@app.route('/api-login', methods=['POST'])
+def login():
+
+	error=None
+	if not request.json or not 'username' in request.json or not 'password' in request.json:
+		error="Please enter both your username and password!"
+	
+	elif 1>0:
+		error="User does not exist!"
+	
+	elif 2>0:
+		error="Wrong password!"
+	
+	else:
+		return {"_id":"id", "error":None}
+	
+
+
